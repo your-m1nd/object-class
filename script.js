420e8b0fe369b5fd6fe7faf8834e4567d850fbd1
your-m1nd <vi.neurogirl@gmail.com>
2023-07-03 22:21:01 +0300

    add html, script

---
+++ script.js
@@ -0,0 +1,94 @@
+//Задание 1
+
+class Worker {
+    constructor(name, surname, rate, days) {
+        this.name = name;
+        this.surname = surname;
+				this.rate = rate;
+				this.days = days;
+    }
+    getSalary(days, rate) {
+        return this.rate * this.days;
+    }
+}
+
+var worker = new Worker('Иван', 'Иванов', 10, 31);
+
+console.log(worker.name); //выведет 'Иван'
+console.log(worker.surname); //выведет 'Иванов'
+console.log(worker.rate); //выведет 10
+console.log(worker.days); //выведет 31
+console.log(worker.getSalary()); //выведет 310 - то есть 10*31
+
+//Задание 2 
+const data = [
+    {
+      id: 1,
+      type: 'car',
+      brand: 'Audi',
+      doors: 4,
+      price: 4300000,
+      image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/2020_Audi_e-Tron_Sport_50_Quattro.jpg/1200px-2020_Audi_e-Tron_Sport_50_Quattro.jpg'
+    },
+    {
+      id: 2,
+      type: 'car',
+      brand: 'Mercedes-Benz',
+      doors: 4,
+      price: 2800000,
+      image: 'https://www.mercedes-benz-mena.com/en/passengercars/mercedes-benz-cars/models/e-class/sedan-w213-fl/explore/_jcr_content/notificationboxes/notificationbox/image.MQ6.12.20211013084329.jpeg'
+    },
+      {
+      id: 3,
+      type: 'bike',
+      brand: 'Harley-Davidson',
+      maxSpeed: 210,
+      price: 1300000,
+      image: 'https://www.harley-davidson.com/content/dam/h-d/images/product-images/bikes/motorcycle/2022/2022-iron-883/2022-iron-883-016/2022-iron-883-016-motorcycle.jpg'
+    },
+    {
+      id: 4,
+      type: 'bike',
+      brand: 'Harley-Davidson',
+      maxSpeed: 220,
+      price: 1400000,
+      image: 'https://cdn.dealerspike.com/imglib/products/harley-showroom/2020/livewire/main/Vivid-Black-Main.png'
+    }
+  ];
+
+class Transport {
+    constructor (type, price, brand, image) {
+        this.type = type;
+        this.price = price;
+        this.brand = brand;
+    }
+
+    getInfo() {
+        return {
+        type: this.type,
+        brand: this.brand,
+        }
+    }
+
+    getPrice() {
+        return {
+        price: this.price,
+        }
+    }
+}
+
+class Car extends Transport {
+    constructor () {
+
+    }
+
+    getDoorsCount()
+}
+
+class Bike extends Transport {
+    constructor () {}
+    getMaxSpeed() {}
+}
+
+
+
